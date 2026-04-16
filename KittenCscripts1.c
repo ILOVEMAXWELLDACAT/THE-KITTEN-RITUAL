673606e87cb805972d4a5ffd0b59919d61715842
ILOVEMAXWELLDACAT <cats44504@gmail.com>
2026-04-16 01:11:07 +0000

    Add code files

---
+++ KittenCscripts1.c
@@ -0,0 +1,15 @@
+#import <stdio.h>
+int main() {
+    printf("its here...\n");
+    printf(" /\\__/\\\n");
+    printf("(  ^_ ^  )\n");
+    printf(" \\     / \n");
+    printf("  )   (  //\n");
+    printf(" /     \\//\n");
+    printf(" |     |\n");
+    printf(" || ||  |\n");
+    printf(" || || |\n");
+    printf(" || || \n");
+    printf("(_) (_)\n");
+    return 0;
+}
---
+++ KittenGodScripts.c
@@ -0,0 +1,17 @@
+#import <stdio.h>
+int main() {
+    printf("    ____       ")
+    printf("    |   |
+    printf("    |   |\n");
+    printf("____|   |____)
+    printf("|            |\n");
+    printf("|            |\n");
+    printf("----    ---- \n");
+    printf("   |    |\n");
+    printf("   |    |\n");
+    printf("   |    |\n");
+    printf("   |    |\n");
+    printf("   ------\n")
+    return 0;
+}
+
